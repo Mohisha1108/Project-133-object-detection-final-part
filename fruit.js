f7a69155fcba5bcc3740497b375e57dcbceaf316
Mohisha1108 <73828119+Mohisha1108@users.noreply.github.com>
2021-07-19 15:53:41 +0530

    Add files via upload

---
+++ fruit.js
@@ -0,0 +1,15 @@
+function bedroom(){
+    window.location = 'bedroom.html';
+}
+function bottles(){
+    window.location = 'bottles.html';
+}
+function desk(){
+    window.location = 'desk.html';
+}
+function fruitbasket(){
+    window.location = 'fruitbasket.html';
+}
+function tvac(){
+    window.location = 'tvac.html';
+}
